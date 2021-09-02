0cdd12046d855db3fe73276b9940bd43d7c66c80
Noah Lottig <nrlottig@wisc.edu>
2021-09-02 10:23:25 -0500

    misc updates

--- clean_wqp_processed_data.R
+++ clean_wqp_processed_data.R
@@ -2,27 +2,37 @@ rm(list=ls())
 library(tidyverse)
 library(lubridate)
 library(fs)
+library(RPostgreSQL)
 
+#info for connecting to postgres DB
+dbDisconnect(con)
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, dbname = "lagos_us_limno",
+                 host = '144.92.62.199', port = 5432,
+                 user = "postgres", password = 'SparklingRusty')
+
+#read in the datafiles
 data_dir <- "~/Dropbox/CL_LAGOSUS_exports/LAGOSUS_LIMNO/US_NEW"
 dat <- data_dir %>% 
     dir_ls(regexp = "\\.csv$") %>% 
     map_dfr(~read_csv(.x, col_types = cols(.default = "c")), .id = "source")
 
-column_drop <- c("X","OrganizationIdentifier","OrganizationFormalName","ActivityIdentifier","ActivityTypeCode",'ActivityMediaName',"ActivityMediaSubdivisionName","ActivityStartTime.Time","ActivityStartDateTime","ActivityStartTime.TimeZoneCode","ActivityEndDate","ActivityEndTime.Time",
-                 "ActivityEndTime.TimeZoneCode","ActivityDepthHeightMeasure.MeasureUnitCode","ActivityDepthAltitudeReferencePointText","ActivityTopDepthHeightMeasure.MeasureUnitCode",
-                 "ActivityBottomDepthHeightMeasure.MeasureValue","ActivityBottomDepthHeightMeasure.MeasureUnitCode","ProjectIdentifier","ActivityConductingOrganizationText",
-                 "ActivityCommentText","HydrologicCondition","HydrologicEvent","SampleCollectionMethod.MethodIdentifier","SampleCollectionMethod.MethodName",
-                 "SampleCollectionEquipmentName","ResultDetectionConditionText","ResultSampleFractionText","ResultStatusIdentifier","StatisticalBaseCode","ResultValueTypeName","ResultTemperatureBasisText",
-                 "ResultCommentText","USGSPCode","ResultAnalyticalMethod.MethodIdentifier","ResultAnalyticalMethod.MethodIdentifierContext","AnalysisStartDate","ResultLaboratoryCommentText","DetectionQuantitationLimitTypeName",
-                 "DetectionQuantitationLimitMeasure.MeasureValue","DetectionQuantitationLimitMeasure.MeasureUnitCode","PreparationStartDate","ProviderName","SampleAquifer","ResultMeasure.MeasureUnitCode")
+# column_drop <- c("X","OrganizationIdentifier","OrganizationFormalName","ActivityIdentifier","ActivityTypeCode",'ActivityMediaName',"ActivityMediaSubdivisionName","ActivityStartTime.Time","ActivityStartDateTime","ActivityStartTime.TimeZoneCode","ActivityEndDate","ActivityEndTime.Time",
+#                  "ActivityEndTime.TimeZoneCode","ActivityDepthHeightMeasure.MeasureUnitCode","ActivityDepthAltitudeReferencePointText","ActivityTopDepthHeightMeasure.MeasureUnitCode",
+#                  "ActivityBottomDepthHeightMeasure.MeasureValue","ActivityBottomDepthHeightMeasure.MeasureUnitCode","ProjectIdentifier","ActivityConductingOrganizationText",
+#                  "ActivityCommentText","HydrologicCondition","HydrologicEvent","SampleCollectionMethod.MethodIdentifier","SampleCollectionMethod.MethodName",
+#                  "SampleCollectionEquipmentName","ResultDetectionConditionText","ResultSampleFractionText","ResultStatusIdentifier","StatisticalBaseCode","ResultValueTypeName","ResultTemperatureBasisText",
+#                  "ResultCommentText","USGSPCode","ResultAnalyticalMethod.MethodIdentifier","ResultAnalyticalMethod.MethodIdentifierContext","AnalysisStartDate","ResultLaboratoryCommentText","DetectionQuantitationLimitTypeName",
+#                  "DetectionQuantitationLimitMeasure.MeasureValue","DetectionQuantitationLimitMeasure.MeasureUnitCode","PreparationStartDate","ProviderName","SampleAquifer","ResultMeasure.MeasureUnitCode")
 
-
+#reduce source column
+dat$source <- sapply(strsplit(dat$source, split= "/", fixed = TRUE), tail, 1L)
 
 #rename columns to fit LAGOS Schema
 dat2 <- dat %>% 
     rename(sampledate = ActivityStartDate) %>% 
-    rename(sampledepth_m_legacy = ActivityDepthHeightMeasure.MeasureValue) %>% 
-    rename(alt_sampledepth =ResultDepthHeightMeasure.MeasureValue) %>% 
+    rename(sampledepth_m_legacy = ActivityDepthHeightMeasure.MeasureValue) %>% #depth at which the sample was taken
+    rename(alt_sampledepth =ResultDepthHeightMeasure.MeasureValue) %>% #depth at which the result was derived from
     rename(datavalue = ResultMeasureValue) %>% 
     rename(qualifier_legacy = DetectionQuantitationLimitTypeName) %>% 
     rename(labmethodname_legacy = ResultAnalyticalMethod.MethodName) %>% 
@@ -47,49 +57,66 @@ dat2 <- dat2 %>% mutate_at(c('sampledepth_m_legacy','alt_sampledepth'),as.numeri
 dat2$sampleposition_legacy <- ifelse(is.na(dat2$sampledepth_m_legacy) & is.na(dat2$alt_sampledepth),"UNKNOWN","SPECIFIED")
 dat2$sampledepth_m_legacy <- ifelse(dat2$sampleposition_legacy == 'SPECIFIED' & is.na(dat2$sampledepth_m_legacy), dat2$alt_sampledepth, dat2$sampledepth_m_legacy)
 dat2 <- dat2 %>% select(-alt_sampledepth)
+
+#update this with new names
 dat2$sampledepth_m_legacy[which(dat2$CharacteristicName=="Depth, Secchi disk depth" | dat2$CharacteristicName=="Secchi, Horizontal Distance")] <- 0
 
 #Assign sample types based on equipment used
-unique(dat2$SampleCollectionEquipmentName)
-grab_equip = c("Grab sample","Bucket","Open-Mouth Bottle","Sampler, frame-type, Teflon bottle","Syringe","Water Bottle","Secchi Disk","Horizontal Secchi Disk")
-unique(dat2$SampleCollectionMethod.MethodIdentifier)
-grab_equip <- c(grab_equip,"GRAB-001","Stainless Bucket","Water Bottle","Grab","NPS_GRAB","SECCHI_DISK","HORIZONTAL_DISK","MIDN_UVA_SPGRAB")
-unique(dat2$SampleCollectionMethod.MethodName)
-grab_equip <- c(grab_equip,"Grab Sample Collected With A Stainless Bucket","SRBC Standard Grab Sample Method","Grab",
-                "Grab Sample Collected With A Water Bottle","Grab sample  (dip)","Water Grab Sampling","Grab sample collection",
-                "Unspecified Standard Grab Sample Procedure")
+sample_equip <- c(unique(dat2$SampleCollectionEquipmentName),
+                  unique(dat2$SampleCollectionMethod.MethodIdentifier),
+                  unique(dat2$SampleCollectionMethod.MethodName),
+                  unique(dat2$SampleCollectionMethod.MethodIdentifierContext))
 
+sample_equip #see the equipment
+
+grab_equip = c("Grab sample","Bucket","Open-Mouth Bottle","Sampler, frame-type, Teflon bottle","Syringe",
+               "Water Bottle","Secchi Disk","Horizontal Secchi Disk","GRAB-001","Stainless Bucket","Water Bottle",
+               "Grab","NPS_GRAB","SECCHI_DISK","HORIZONTAL_DISK","MIDN_UVA_SPGRAB","Grab Sample Collected With A Stainless Bucket",
+               "SRBC Standard Grab Sample Method","Grab","Grab Sample Collected With A Water Bottle",
+               "Grab sample  (dip)","Water Grab Sampling","Grab sample collection",
+               "Unspecified Standard Grab Sample Procedure","USGS parameter code 82398")
+
+sample_equip <- sample_equip[! sample_equip %in% grab_equip] #remove grab samples from list
+
+#assign grab samples to appropriate observations
 dat2$sampletype_legacy<-''
 dat2$sampletype_legacy[which(dat2$SampleCollectionEquipmentName %in% grab_equip)] <- "GRAB"
 dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodIdentifier %in% grab_equip)] <- "GRAB"
 dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodName %in% grab_equip)] <- "GRAB"
+dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodIdentifierContext %in% grab_equip)] <- "GRAB"
+sample_equip
 
-unique(dat2$SampleCollectionEquipmentName)
-depth_equip <- c("Van Dorn Bottle","Peristaltic pump","Pump/Submersible","Submersible gear pump","Probe/Sensor","Van Dorn sampler","Pump/Non-Submersible")
-unique(dat2$SampleCollectionMethod.MethodIdentifier)
-depth_eqip <- c(depth_equip)
-unique(dat2$SampleCollectionMethod.MethodName)
-depth_eqip <- c(depth_equip,"Suction lift peristaltic pump")
+depth_equip <- c("Van Dorn Bottle","Peristaltic pump","Pump/Submersible","Submersible gear pump",
+                 "Probe/Sensor","Van Dorn sampler","Pump/Non-Submersible","Suction lift peristaltic pump")
+
+sample_equip <- sample_equip[! sample_equip %in% depth_equip] #remove depth specific samples from list
 
 dat2$sampletype_legacy[which(dat2$sampletype_legacy=="" & dat2$SampleCollectionEquipmentName %in% depth_equip)] <- "DEPTH"
 dat2$sampletype_legacy[which(dat2$sampletype_legacy=="" & dat2$SampleCollectionMethod.MethodIdentifier %in% depth_equip)] <- "DEPTH"
 dat2$sampletype_legacy[which(dat2$sampletype_legacy=="" & dat2$SampleCollectionMethod.MethodName %in% depth_equip)] <- "DEPTH"
+dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodIdentifierContext %in% depth_equip)] <- "DEPTH"
 
-unique(dat2$SampleCollectionEquipmentName)
-int_equip <- c()
-unique(dat2$SampleCollectionMethod.MethodIdentifier)
-int_equip <- c(int_equip,"2meterMPCA")
-unique(dat2$SampleCollectionMethod.MethodName)
-int_equip <- c(int_equip,"Integrated water sampler")
+sample_equip
+
+int_equip <- c("2meterMPCA","Integrated water sampler")
+sample_equip <- sample_equip[! sample_equip %in% int_equip] #remove integrated samples from list
+
 
 dat2$sampletype_legacy[which(dat2$SampleCollectionEquipmentName %in% int_equip)] <- "INTEGRATED"
 dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodIdentifier %in% int_equip)] <- "INTEGRATED"
 dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodName %in% int_equip)] <- "INTEGRATED"
+dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodIdentifierContext %in% int_equip)] <- "INTEGRATED"
+sample_equip
+#make sure no obvious sampling devices were missed
 
 dat2$sampletype_legacy[which(dat2$sampletype_legacy=="")] <- "UNKNOWN"
 
+
 dat2$sampledepth_m_lagos <- dat2$sampledepth_m_legacy
 dat2$samplelayer_lagos <- dat2$sampleposition_legacy
+
+#assign sample positions based on type of sample (secchi) or type of gear used
+#update with new variable names
 dat2$samplelayer_lagos[which(dat2$CharacteristicName=="Depth, Secchi disk depth" | dat2$CharacteristicName=="Secchi, Horizontal Distance")] <- "EPI"
 
 #assign grab and integrated samples as EPI when legacy is unknown or not specified
@@ -102,13 +129,32 @@ dat2$LaboratoryName <- NULL
 
 dat3 <- dat2 %>% filter(samplelayer_lagos=="UNKNOWN")
 
+
+#Link to LAGOSLAKEID sites
+
+lagos_sites_us <- dbGetQuery(con,'select * from limno.lagoslakesites', stringsAsFactors = FALSE) %>% 
+    select(samplesiteid_legacy,lagoslakeid,samplesiteid_lagos)
+
+dat2 <- dat2 %>% left_join(lagos_sites_us)
+# dat3 <- dat2 %>% filter(is.na(lagoslakeid)) %>% 
+#     select(source,OrganizationIdentifier,samplesiteid_legacy) %>% 
+#     distinct()
+# write_csv(dat3,"nolink.csv")
+# dat4 <- dat2 %>% filter(!is.na(lagoslakeid)) %>% 
+#     select(source,OrganizationIdentifier,samplesiteid_legacy) %>% 
+#     distinct()
+# write_csv(dat4,"linked.csv")
+
+
+lagos_variables <- dbGetQuery(con,'select * from limno.lagosvariables_us', stringsAsFactors = FALSE)
+
+write_csv(lagos_variables,"lagos_variables.csv")
 #columns to add
 #variableid_lagos
 #programtableid_lagos
 #comments_lagos
 #censorcode_flag_lagos
-#lagoslakeid
-#samplesiteid_lagos
+
 #lakeid_legacy
 #methodqualifier_legacy
 #flag_lagos
