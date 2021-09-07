930c9a611b1a43fa4ab7b889d27719df98848f62
Noah Lottig <nrlottig@wisc.edu>
2021-09-07 14:55:23 -0500

    updates to processing script

--- clean_wqp_processed_data.R
+++ clean_wqp_processed_data.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(lubridate)
 library(fs)
 library(RPostgreSQL)
+library(robustbase)
 
 #info for connecting to postgres DB
 dbDisconnect(con)
@@ -13,9 +14,14 @@ con <- dbConnect(drv, dbname = "lagos_us_limno",
 
 #read in the datafiles
 data_dir <- "~/Dropbox/CL_LAGOSUS_exports/LAGOSUS_LIMNO/US_NEW"
-dat <- data_dir %>% 
-    dir_ls(regexp = "\\.csv$") %>% 
-    map_dfr(~read_csv(.x, col_types = cols(.default = "c")), .id = "source")
+#list files
+files <- data_dir %>% dir_ls(regexp = "\\.csv$")
+files
+dat <- read_csv(files[5], col_types = cols(.default = "c"))
+
+# dat <- data_dir %>% 
+    # dir_ls(regexp = "\\.csv$") %>% 
+    # map_dfr(~read_csv(.x, col_types = cols(.default = "c")), .id = "source")
 
 # column_drop <- c("X","OrganizationIdentifier","OrganizationFormalName","ActivityIdentifier","ActivityTypeCode",'ActivityMediaName',"ActivityMediaSubdivisionName","ActivityStartTime.Time","ActivityStartDateTime","ActivityStartTime.TimeZoneCode","ActivityEndDate","ActivityEndTime.Time",
 #                  "ActivityEndTime.TimeZoneCode","ActivityDepthHeightMeasure.MeasureUnitCode","ActivityDepthAltitudeReferencePointText","ActivityTopDepthHeightMeasure.MeasureUnitCode",
@@ -40,8 +46,8 @@ dat2 <- dat %>%
     rename(samplesiteid_legacy = MonitoringLocationIdentifier)
 
 
-dat2 <- dat2 %>% unite('comments_legacy', MeasureQualifierCode,  ResultLaboratoryCommentText, sep = '$', remove = FALSE) %>% 
-    select(-MeasureQualifierCode,-ResultLaboratoryCommentText) %>% 
+dat2 <- dat2 %>% unite('comments_legacy', MeasureQualifierCode,  ResultLaboratoryCommentText, ResultCommentText,ActivityCommentText, sep = '$', remove = FALSE) %>% 
+    # select(-MeasureQualifierCode,-ResultLaboratoryCommentText) %>% 
     mutate(comments_legacy = str_replace_all(comments_legacy,'NA','')) %>% 
     mutate(comments_legacy = str_replace_all(comments_legacy,'\\$',' '))
 
@@ -50,7 +56,7 @@ unique(dat2$comments_legacy)
 
 
 #Assign Depths
-unique(dat2$sampledepth_m_legacy)
+unique(as.numeric(dat2$sampledepth_m_legacy))
 unique(as.numeric(dat2$alt_sampledepth))
 dat2 <- dat2 %>% mutate_at(c('sampledepth_m_legacy','alt_sampledepth'),as.numeric)
 
@@ -59,7 +65,8 @@ dat2$sampledepth_m_legacy <- ifelse(dat2$sampleposition_legacy == 'SPECIFIED' & 
 dat2 <- dat2 %>% select(-alt_sampledepth)
 
 #update this with new names
-dat2$sampledepth_m_legacy[which(dat2$CharacteristicName=="Depth, Secchi disk depth" | dat2$CharacteristicName=="Secchi, Horizontal Distance")] <- 0
+
+dat2$sampledepth_m_legacy[which(dat2$CharacteristicName=="Secchi" & is.na(dat2$sampledepth_m_legacy))] <- 0
 
 #Assign sample types based on equipment used
 sample_equip <- c(unique(dat2$SampleCollectionEquipmentName),
@@ -87,7 +94,8 @@ dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodIdentifierContext
 sample_equip
 
 depth_equip <- c("Van Dorn Bottle","Peristaltic pump","Pump/Submersible","Submersible gear pump",
-                 "Probe/Sensor","Van Dorn sampler","Pump/Non-Submersible","Suction lift peristaltic pump")
+                 "Probe/Sensor","Van Dorn sampler","Pump/Non-Submersible","Suction lift peristaltic pump",
+                 "Van Dorn bottle","multiprobesonde","Multi-Probe Sonde" )
 
 sample_equip <- sample_equip[! sample_equip %in% depth_equip] #remove depth specific samples from list
 
@@ -115,19 +123,104 @@ dat2$sampletype_legacy[which(dat2$sampletype_legacy=="")] <- "UNKNOWN"
 dat2$sampledepth_m_lagos <- dat2$sampledepth_m_legacy
 dat2$samplelayer_lagos <- dat2$sampleposition_legacy
 
+#examine unknown depth location samples
+#basic checks to see how much data exists, what the programs, see if there is anything we need to really figure out
+temp <- dat2 %>% filter(sampletype_legacy=="UNKNOWN") %>% filter(is.na(sampledepth_m_legacy))
+
+#look at programs
+table(temp$OrganizationFormalName)
+
+#look at approaches for programs with lots of samples
+temp2 <- temp %>% filter(OrganizationFormalName=="USGS Virginia Water Science Center")
+
+sample_equip_temp <- c(unique(temp2$SampleCollectionEquipmentName),
+                  unique(temp2$SampleCollectionMethod.MethodIdentifier),
+                  unique(temp2$SampleCollectionMethod.MethodName),
+                  unique(temp2$SampleCollectionMethod.MethodIdentifierContext))
+sample_equip_temp
+
+unique(temp2$ResultCommentText)
+
+
+
 #assign sample positions based on type of sample (secchi) or type of gear used
-#update with new variable names
-dat2$samplelayer_lagos[which(dat2$CharacteristicName=="Depth, Secchi disk depth" | dat2$CharacteristicName=="Secchi, Horizontal Distance")] <- "EPI"
+dat2$samplelayer_lagos[which(dat2$CharacteristicName=="Secchi")] <- "EPI"
 
 #assign grab and integrated samples as EPI when legacy is unknown or not specified
 dat2$samplelayer_lagos[which(dat2$samplelayer_lagos=="UNKNOWN" & dat2$sampletype_legacy=="GRAB")] <- "EPI"
+dat2$samplelayer_lagos[which(dat2$samplelayer_lagos=="UNKNOWN" & dat2$ResultCommentText=="Surface")] <- "EPI"
+
+#note: may want to revist this
 dat2$samplelayer_lagos[which(dat2$samplelayer_lagos=="UNKNOWN" & dat2$sampletype_legacy=="INTEGRATED")] <- "EPI"
 
 dat2$sampletype_lagos <- dat2$sampletype_legacy
-dat2$LaboratoryName <- NULL
+
+#look at 
+dat3 <- dat2 %>% filter(sampletype_lagos=="UNKNOWN" & is.na(sampledepth_m_legacy))
+write_csv(dat3,"unknownlocations.csv")
+#make sure we aren't dropping anything important
+unique(dat3$samplelayer_lagos)
+unique(dat3$sampletype_lagos)
+
+#drop samples for which we can't determine a depth or sample
+dat2 <- dat2 %>% filter(paste0(sampledepth_m_legacy,sampletype_lagos)!="NAUNKNOWN")
+nrow(dat)-nrow(dat2) #should match dat3 rows
 
 
-dat3 <- dat2 %>% filter(samplelayer_lagos=="UNKNOWN")
+
+################ QAQC #################
+
+#parameter Mapping
+params_maps<- dat2 %>% select(CharacteristicName, source_parameter) %>% distinct()
+params_maps
+
+#check to make sure no weird sample fractions and activity types made it in
+unique(dat2$ActivityTypeCode)
+unique(dat2$ResultSampleFractionText)
+
+dat2 <- dat2 %>% filter(source_parameter!="Depth")
+
+
+
+params <- unique(dat2$CharacteristicName)
+
+
+#create histograms
+pdf(file="graphics/VA_histograms.pdf",width=8.5,height=11)
+par(mfrow=c(3,2))
+
+for(i in 1:length(params)) {
+    temp <- (as.numeric(dat2$datavalue))[which(dat2$CharacteristicName==params[i])]
+    if(length(temp>100)) 
+        {
+        adjbox(x = temp ,main=params[i],notch=TRUE)
+        mtext(side=3,adj=0.1,paste("n=",length(temp))) 
+    } else 
+                  {next()}
+}
+dev.off()
+
+pdf(file="graphics/VA_distributions.pdf",width=8.5,height=11)
+par(mfrow=c(3,2))
+
+for(i in 1:length(params)) {
+    temp <- log10(as.numeric(dat2$datavalue)+1)[which(dat2$CharacteristicName==params[i])]
+    if(length(temp>100)) 
+    {
+        d <- density(x = temp,na.rm=T)
+        plot(d, main=params[i])
+    } else 
+    {next()}
+}
+dev.off()
+
+
+
+
+
+
+
+
 
 
 #Link to LAGOSLAKEID sites
