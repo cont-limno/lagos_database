bfc3430671a9f411104b0a8930efd21a28e9c387
Noah Lottig <nrlottig@wisc.edu>
2021-09-23 15:33:15 -0500

    updates to processing script

--- clean_wqp_processed_data.R
+++ clean_wqp_processed_data.R
@@ -6,34 +6,31 @@ library(RPostgreSQL)
 library(robustbase)
 
 #info for connecting to postgres DB
+{
 dbDisconnect(con)
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname = "lagos_us_limno",
                  host = '144.92.62.199', port = 5432,
                  user = "postgres", password = 'SparklingRusty')
-
+}
 #read in the datafiles
+
 data_dir <- "~/Dropbox/CL_LAGOSUS_exports/LAGOSUS_LIMNO/US_NEW"
 #list files
 files <- data_dir %>% dir_ls(regexp = "\\.csv$")
 files
-dat <- read_csv(files[5], col_types = cols(.default = "c"))
-
-# dat <- data_dir %>% 
-    # dir_ls(regexp = "\\.csv$") %>% 
-    # map_dfr(~read_csv(.x, col_types = cols(.default = "c")), .id = "source")
-
-# column_drop <- c("X","OrganizationIdentifier","OrganizationFormalName","ActivityIdentifier","ActivityTypeCode",'ActivityMediaName',"ActivityMediaSubdivisionName","ActivityStartTime.Time","ActivityStartDateTime","ActivityStartTime.TimeZoneCode","ActivityEndDate","ActivityEndTime.Time",
-#                  "ActivityEndTime.TimeZoneCode","ActivityDepthHeightMeasure.MeasureUnitCode","ActivityDepthAltitudeReferencePointText","ActivityTopDepthHeightMeasure.MeasureUnitCode",
-#                  "ActivityBottomDepthHeightMeasure.MeasureValue","ActivityBottomDepthHeightMeasure.MeasureUnitCode","ProjectIdentifier","ActivityConductingOrganizationText",
-#                  "ActivityCommentText","HydrologicCondition","HydrologicEvent","SampleCollectionMethod.MethodIdentifier","SampleCollectionMethod.MethodName",
-#                  "SampleCollectionEquipmentName","ResultDetectionConditionText","ResultSampleFractionText","ResultStatusIdentifier","StatisticalBaseCode","ResultValueTypeName","ResultTemperatureBasisText",
-#                  "ResultCommentText","USGSPCode","ResultAnalyticalMethod.MethodIdentifier","ResultAnalyticalMethod.MethodIdentifierContext","AnalysisStartDate","ResultLaboratoryCommentText","DetectionQuantitationLimitTypeName",
-#                  "DetectionQuantitationLimitMeasure.MeasureValue","DetectionQuantitationLimitMeasure.MeasureUnitCode","PreparationStartDate","ProviderName","SampleAquifer","ResultMeasure.MeasureUnitCode")
+dat <- read_csv(files[1], col_types = cols(.default = "c"))
 
 #reduce source column
-dat$source <- sapply(strsplit(dat$source, split= "/", fixed = TRUE), tail, 1L)
+# dat$source <- sapply(strsplit(dat$source, split= "/", fixed = TRUE), tail, 1L)
 
+#link to lagoslakeid
+{
+lagos_link <- read_csv("lagos_wqp_20210920.csv") %>% 
+    rename(MonitoringLocationIdentifier = wqp_monitoringlocationidentifier)
+
+dat <- dat %>% left_join(lagos_link) %>% drop_na(lagoslakeid)
+}
 #rename columns to fit LAGOS Schema
 dat2 <- dat %>% 
     rename(sampledate = ActivityStartDate) %>% 
@@ -43,10 +40,11 @@ dat2 <- dat %>%
     rename(qualifier_legacy = DetectionQuantitationLimitTypeName) %>% 
     rename(labmethodname_legacy = ResultAnalyticalMethod.MethodName) %>% 
     rename(labmethodinfo_legacy = MethodDescriptionText) %>% 
-    rename(samplesiteid_legacy = MonitoringLocationIdentifier)
+    rename(samplesiteid_legacy = MonitoringLocationIdentifier) %>% 
+    rename(qualifier_name_legacy = ResultLaboratoryCommentText)
 
 
-dat2 <- dat2 %>% unite('comments_legacy', MeasureQualifierCode,  ResultLaboratoryCommentText, ResultCommentText,ActivityCommentText, sep = '$', remove = FALSE) %>% 
+dat2 <- dat2 %>% unite('comments_legacy', MeasureQualifierCode,  ResultCommentText,ActivityCommentText, sep = '$', remove = FALSE) %>% 
     # select(-MeasureQualifierCode,-ResultLaboratoryCommentText) %>% 
     mutate(comments_legacy = str_replace_all(comments_legacy,'NA','')) %>% 
     mutate(comments_legacy = str_replace_all(comments_legacy,'\\$',' '))
@@ -55,17 +53,16 @@ unique(dat2$comments_legacy)
 #check on this field with austin "ResultLaboratoryCommentCode"
 
 
-#Assign Depths
-unique(as.numeric(dat2$sampledepth_m_legacy))
-unique(as.numeric(dat2$alt_sampledepth))
+#Assign Depths and make sure there are no non-numeric values
+unique(dat2$sampledepth_m_legacy[is.na(as.numeric(dat2$sampledepth_m_legacy))])
+unique(dat2$alt_sampledepth[is.na(as.numeric(dat2$alt_sampledepth))])
 dat2 <- dat2 %>% mutate_at(c('sampledepth_m_legacy','alt_sampledepth'),as.numeric)
 
 dat2$sampleposition_legacy <- ifelse(is.na(dat2$sampledepth_m_legacy) & is.na(dat2$alt_sampledepth),"UNKNOWN","SPECIFIED")
 dat2$sampledepth_m_legacy <- ifelse(dat2$sampleposition_legacy == 'SPECIFIED' & is.na(dat2$sampledepth_m_legacy), dat2$alt_sampledepth, dat2$sampledepth_m_legacy)
 dat2 <- dat2 %>% select(-alt_sampledepth)
 
-#update this with new names
-
+#assing secchi to depth of 0
 dat2$sampledepth_m_legacy[which(dat2$CharacteristicName=="Secchi" & is.na(dat2$sampledepth_m_legacy))] <- 0
 
 #Assign sample types based on equipment used
@@ -123,6 +120,9 @@ dat2$sampletype_legacy[which(dat2$sampletype_legacy=="")] <- "UNKNOWN"
 dat2$sampledepth_m_lagos <- dat2$sampledepth_m_legacy
 dat2$samplelayer_lagos <- dat2$sampleposition_legacy
 
+
+################### QAQC WHILE PROCESSING
+
 #examine unknown depth location samples
 #basic checks to see how much data exists, what the programs, see if there is anything we need to really figure out
 temp <- dat2 %>% filter(sampletype_legacy=="UNKNOWN") %>% filter(is.na(sampledepth_m_legacy))
@@ -141,7 +141,7 @@ sample_equip_temp
 
 unique(temp2$ResultCommentText)
 
-
+################# END QAQC ############################
 
 #assign sample positions based on type of sample (secchi) or type of gear used
 dat2$samplelayer_lagos[which(dat2$CharacteristicName=="Secchi")] <- "EPI"
@@ -155,12 +155,14 @@ dat2$samplelayer_lagos[which(dat2$samplelayer_lagos=="UNKNOWN" & dat2$sampletype
 
 dat2$sampletype_lagos <- dat2$sampletype_legacy
 
+
+######### QAQC TO EXAMINE UNKOWN LOCATION DEPTH AND SEE IF SOMETHING CAN BE FIXED ####################
 #look at 
 dat3 <- dat2 %>% filter(sampletype_lagos=="UNKNOWN" & is.na(sampledepth_m_legacy))
 write_csv(dat3,"unknownlocations.csv")
 #make sure we aren't dropping anything important
-unique(dat3$samplelayer_lagos)
-unique(dat3$sampletype_lagos)
+
+############# END QAQC
 
 #drop samples for which we can't determine a depth or sample
 dat2 <- dat2 %>% filter(paste0(sampledepth_m_legacy,sampletype_lagos)!="NAUNKNOWN")
@@ -178,7 +180,61 @@ params_maps
 unique(dat2$ActivityTypeCode)
 unique(dat2$ResultSampleFractionText)
 
+temp <- dat2 %>% filter(ActivityTypeCode ==  "Sample-Integrated Vertical Profile")
+
 dat2 <- dat2 %>% filter(source_parameter!="Depth")
+
+#merge in variable id values
+lagos_variables <- dbGetQuery(con,'select * from limno.lagosvariables_us', stringsAsFactors = FALSE) %>% 
+    select(variableid_lagos,variablename)
+
+dat2 <- dat2 %>% left_join(lagos_variables,by=c("CharacteristicName" = "variablename"))
+rm(lagos_variables)
+
+#create columns needed for the postgres table
+dat2$qualifier_legacy_full <- dat2$comments_legacy
+dat2 <- dat2 %>% 
+    mutate(programtableid_lagos = NA,
+           comments_lagos = NA, # set to null
+           censorcode_flag_lagos = NA, #set to null
+           samplesiteid_lagos_pre_cluster = NA, #set to null
+           labemethodname_legacy = NA, #set to null
+           lakeid_legacy = NA, #set to null
+           methodqualifier_legacy = NA, #this is where we have identified corrected vs uncorrected
+           version_comment_lagos = NA, #set to null
+           flag_lagos = NA, #set to null
+           varshortname_comment_lagos = NA, #set to null
+           lakeid_nhdid = NA, #set to null
+           depth_comment_lagos = NA, #set to null
+           primarysamplesite_flag_lagos = NA, #set to null
+           valueid_lagos = NA, #set to null
+           sitecordid_lagos = NA, #set to null
+           samplesiteid_lagos = NA, #set to null
+           programid_lagos_us = NA, #set to null
+           qualifier_legacy_full_cleaned = NA, #set to null
+           qualifier_detect_info = NA, #set to null
+           lagos_min_depth = NA, #set to null
+           legacy_min_depth = NA, #set to null
+           sample_delta = NA, #set to null
+           epi_depth = NA, #set to null
+           lagos_epi_assignment = NA, #set ot null
+           exported_epi_new = NA, #set to null
+           exported_eventid_epi = NA #set to null)
+    )
+
+dat3 <- dat2 %>% 
+    select(sampledate,variableid_lagos,CharacteristicName,sampledepth_m_legacy,datavalue,programtableid_lagos,sampletype_legacy,sampledepth_m_lagos,
+           samplelayer_lagos,sampletype_lagos,comments_legacy,comments_lagos,censorcode_flag_lagos,samplesiteid_legacy,lagoslakeid,
+           samplesiteid_lagos_pre_cluster,labemethodname_legacy,lakeid_legacy,labmethodinfo_legacy,methodqualifier_legacy,
+           version_comment_lagos,flag_lagos,varshortname_comment_lagos,lakeid_nhdid,depth_comment_lagos,primarysamplesite_flag_lagos,
+           valueid_lagos,sitecordid_lagos,samplesiteid_lagos,programid_lagos_us,source_value, source_unit, source_parameter, 
+           qualifier_legacy,qualifier_name_legacy,
+           detectionlimit_legacy,detectionlimit_unit_legacy,qualifier_legacy_full,qualifier_legacy_full_cleaned,qualifier_detect_info,
+           lagos_min_depth,legacy_min_depth,sample_delta,epi_depth,lagos_epi_assignment,exported_epi_new,exported_eventid_epi)
+         )
+
+
+
 
 
 
