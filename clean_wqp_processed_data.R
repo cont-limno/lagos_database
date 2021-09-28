9360389e086a3a665789f050d51809a0a2e19fed
Noah Lottig <nrlottig@wisc.edu>
2021-09-28 10:14:26 -0500

    misc updates after getting new exports

--- clean_wqp_processed_data.R
+++ clean_wqp_processed_data.R
@@ -6,20 +6,21 @@ library(RPostgreSQL)
 library(robustbase)
 
 #info for connecting to postgres DB
-{
+
 dbDisconnect(con)
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname = "lagos_us_limno",
                  host = '144.92.62.199', port = 5432,
                  user = "postgres", password = 'SparklingRusty')
-}
+
 #read in the datafiles
 
 data_dir <- "~/Dropbox/CL_LAGOSUS_exports/LAGOSUS_LIMNO/US_NEW"
 #list files
 files <- data_dir %>% dir_ls(regexp = "\\.csv$")
 files
-dat <- read_csv(files[1], col_types = cols(.default = "c"))
+files[2]
+dat <- read_csv(files[2], col_types = cols(.default = "c"))
 
 #reduce source column
 # dat$source <- sapply(strsplit(dat$source, split= "/", fixed = TRUE), tail, 1L)
@@ -49,8 +50,6 @@ dat2 <- dat2 %>% unite('comments_legacy', MeasureQualifierCode,  ResultCommentTe
     mutate(comments_legacy = str_replace_all(comments_legacy,'NA','')) %>% 
     mutate(comments_legacy = str_replace_all(comments_legacy,'\\$',' '))
 
-unique(dat2$comments_legacy)
-#check on this field with austin "ResultLaboratoryCommentCode"
 
 
 #Assign Depths and make sure there are no non-numeric values
@@ -71,16 +70,17 @@ sample_equip <- c(unique(dat2$SampleCollectionEquipmentName),
                   unique(dat2$SampleCollectionMethod.MethodName),
                   unique(dat2$SampleCollectionMethod.MethodIdentifierContext))
 
-sample_equip #see the equipment
-
+#filter out known grab methods
 grab_equip = c("Grab sample","Bucket","Open-Mouth Bottle","Sampler, frame-type, Teflon bottle","Syringe",
                "Water Bottle","Secchi Disk","Horizontal Secchi Disk","GRAB-001","Stainless Bucket","Water Bottle",
                "Grab","NPS_GRAB","SECCHI_DISK","HORIZONTAL_DISK","MIDN_UVA_SPGRAB","Grab Sample Collected With A Stainless Bucket",
                "SRBC Standard Grab Sample Method","Grab","Grab Sample Collected With A Water Bottle",
                "Grab sample  (dip)","Water Grab Sampling","Grab sample collection",
                "Unspecified Standard Grab Sample Procedure","USGS parameter code 82398")
-
 sample_equip <- sample_equip[! sample_equip %in% grab_equip] #remove grab samples from list
+
+sample_equip #see the equipment
+#if grabs still exist, add to list and remove again
 
 #assign grab samples to appropriate observations
 dat2$sampletype_legacy<-''
@@ -88,32 +88,35 @@ dat2$sampletype_legacy[which(dat2$SampleCollectionEquipmentName %in% grab_equip)
 dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodIdentifier %in% grab_equip)] <- "GRAB"
 dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodName %in% grab_equip)] <- "GRAB"
 dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodIdentifierContext %in% grab_equip)] <- "GRAB"
-sample_equip
 
+#filter out known approaches for depth distributed sampling
 depth_equip <- c("Van Dorn Bottle","Peristaltic pump","Pump/Submersible","Submersible gear pump",
                  "Probe/Sensor","Van Dorn sampler","Pump/Non-Submersible","Suction lift peristaltic pump",
-                 "Van Dorn bottle","multiprobesonde","Multi-Probe Sonde" )
+                 "Van Dorn bottle","multiprobesonde","Multi-Probe Sonde","Sonde","sonde01" )
 
 sample_equip <- sample_equip[! sample_equip %in% depth_equip] #remove depth specific samples from list
+
+#look and make sure no depth distributed remain and reprocess if necessary
+sample_equip
 
 dat2$sampletype_legacy[which(dat2$sampletype_legacy=="" & dat2$SampleCollectionEquipmentName %in% depth_equip)] <- "DEPTH"
 dat2$sampletype_legacy[which(dat2$sampletype_legacy=="" & dat2$SampleCollectionMethod.MethodIdentifier %in% depth_equip)] <- "DEPTH"
 dat2$sampletype_legacy[which(dat2$sampletype_legacy=="" & dat2$SampleCollectionMethod.MethodName %in% depth_equip)] <- "DEPTH"
 dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodIdentifierContext %in% depth_equip)] <- "DEPTH"
 
-sample_equip
-
+#filter out integrated sampling equipment
 int_equip <- c("2meterMPCA","Integrated water sampler")
 sample_equip <- sample_equip[! sample_equip %in% int_equip] #remove integrated samples from list
 
+#make sure nothing is left and refilter as needed
+sample_equip
 
 dat2$sampletype_legacy[which(dat2$SampleCollectionEquipmentName %in% int_equip)] <- "INTEGRATED"
 dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodIdentifier %in% int_equip)] <- "INTEGRATED"
 dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodName %in% int_equip)] <- "INTEGRATED"
 dat2$sampletype_legacy[which(dat2$SampleCollectionMethod.MethodIdentifierContext %in% int_equip)] <- "INTEGRATED"
-sample_equip
-#make sure no obvious sampling devices were missed
 
+#assign all remaining to unknown
 dat2$sampletype_legacy[which(dat2$sampletype_legacy=="")] <- "UNKNOWN"
 
 
@@ -180,8 +183,11 @@ params_maps
 unique(dat2$ActivityTypeCode)
 unique(dat2$ResultSampleFractionText)
 
-temp <- dat2 %>% filter(ActivityTypeCode ==  "Sample-Integrated Vertical Profile")
+temp <- dat2 %>% filter(ResultSampleFractionText ==   "Non-Filterable (Particle)")
+################## End QAQC
 
+
+#filter out depth because we don't want it
 dat2 <- dat2 %>% filter(source_parameter!="Depth")
 
 #merge in variable id values
@@ -191,6 +197,34 @@ lagos_variables <- dbGetQuery(con,'select * from limno.lagosvariables_us', strin
 dat2 <- dat2 %>% left_join(lagos_variables,by=c("CharacteristicName" = "variablename"))
 rm(lagos_variables)
 
+#Deal with classifying chlorophyll
+dat2$methodqualifier_legacy = NA #this is where we have identified corrected vs uncorrected
+temp <- dat2 %>% filter(CharacteristicName=="Chlorophyll a")
+unique(temp$source_parameter)
+unique(temp$USGSPCode)
+dat2 <- dat2 %>% mutate(methodqualifier_legacy = ifelse(source_parameter=="Chlorophyll a, corrected for pheophytin","corrected",methodqualifier_legacy))
+dat2 <- dat2 %>% mutate(methodqualifier_legacy = ifelse(source_parameter=="Chlorophyll a, uncorrected for pheophytin","uncorrected",methodqualifier_legacy))
+#deal with usgs pcodes
+dat2 <- dat2 %>% mutate(methodqualifier_legacy = ifelse(is.na(methodqualifier_legacy) & USGSPCode==32209,"corrected",methodqualifier_legacy))
+dat2 <- dat2 %>% mutate(methodqualifier_legacy = ifelse(is.na(methodqualifier_legacy) & USGSPCode==70953,"uncorrected",methodqualifier_legacy))
+
+#look at what is left
+temp <- dat2 %>% filter(source_parameter=="Chlorophyll a" & is.na(methodqualifier_legacy))
+unique(temp$labmethodinfo_legacy)
+unique(temp$labmethodname_legacy)
+unique(temp$ResultAnalyticalMethod.MethodIdentifier)
+unique(temp$ResultAnalyticalMethod.MethodIdentifierContext)
+
+#notes
+#USEPA Method 445 provides equations for both corrected and uncorrected
+
+#assign indeterminate to unknown
+dat2 <- dat2 %>% mutate(methodqualifier_legacy = ifelse(is.na(methodqualifier_legacy) & source_parameter=="Chlorophyll a","unknown",methodqualifier_legacy))
+
+#check to make sure all chlorophylls have been assigned
+temp <- dat2 %>% filter(source_parameter=="Chlorophyll a" & is.na(methodqualifier_legacy))
+
+
 #create columns needed for the postgres table
 dat2$qualifier_legacy_full <- dat2$comments_legacy
 dat2 <- dat2 %>% 
@@ -198,9 +232,7 @@ dat2 <- dat2 %>%
            comments_lagos = NA, # set to null
            censorcode_flag_lagos = NA, #set to null
            samplesiteid_lagos_pre_cluster = NA, #set to null
-           labemethodname_legacy = NA, #set to null
            lakeid_legacy = NA, #set to null
-           methodqualifier_legacy = NA, #this is where we have identified corrected vs uncorrected
            version_comment_lagos = NA, #set to null
            flag_lagos = NA, #set to null
            varshortname_comment_lagos = NA, #set to null
@@ -225,16 +257,17 @@ dat2 <- dat2 %>%
 dat3 <- dat2 %>% 
     select(sampledate,variableid_lagos,CharacteristicName,sampledepth_m_legacy,datavalue,programtableid_lagos,sampletype_legacy,sampledepth_m_lagos,
            samplelayer_lagos,sampletype_lagos,comments_legacy,comments_lagos,censorcode_flag_lagos,samplesiteid_legacy,lagoslakeid,
-           samplesiteid_lagos_pre_cluster,labemethodname_legacy,lakeid_legacy,labmethodinfo_legacy,methodqualifier_legacy,
+           samplesiteid_lagos_pre_cluster,labmethodname_legacy,lakeid_legacy,labmethodinfo_legacy,methodqualifier_legacy,
            version_comment_lagos,flag_lagos,varshortname_comment_lagos,lakeid_nhdid,depth_comment_lagos,primarysamplesite_flag_lagos,
            valueid_lagos,sitecordid_lagos,samplesiteid_lagos,programid_lagos_us,source_value, source_unit, source_parameter, 
            qualifier_legacy,qualifier_name_legacy,
            detectionlimit_legacy,detectionlimit_unit_legacy,qualifier_legacy_full,qualifier_legacy_full_cleaned,qualifier_detect_info,
            lagos_min_depth,legacy_min_depth,sample_delta,epi_depth,lagos_epi_assignment,exported_epi_new,exported_eventid_epi)
-         )
+
+write_csv(dat3,"processed_data/MD_processed.csv")
 
 #dat3 would be the processed file that gets loaded into postgres
-
+#yet todo is add observation id to each based on values already loaded in postgres and push file to postgres
 
 
 
@@ -242,26 +275,24 @@ params <- unique(dat2$CharacteristicName)
 
 
 #create histograms
-pdf(file="graphics/VA_histograms.pdf",width=8.5,height=11)
+pdf(file="graphics/MD_histograms.pdf",width=8.5,height=11)
 par(mfrow=c(3,2))
 
 for(i in 1:length(params)) {
-    temp <- (as.numeric(dat2$datavalue))[which(dat2$CharacteristicName==params[i])]
-    if(length(temp>100)) 
-        {
+    temp <- (as.numeric(dat3$datavalue))[which(dat3$CharacteristicName==params[i])]
+    if(length(temp)>25) {
         adjbox(x = temp ,main=params[i],notch=TRUE)
         mtext(side=3,adj=0.1,paste("n=",length(temp))) 
-    } else 
-                  {next()}
+    } else {next()}
 }
 dev.off()
 
-pdf(file="graphics/VA_distributions.pdf",width=8.5,height=11)
+pdf(file="graphics/MD_distributions.pdf",width=8.5,height=11)
 par(mfrow=c(3,2))
 
 for(i in 1:length(params)) {
     temp <- log10(as.numeric(dat2$datavalue)+1)[which(dat2$CharacteristicName==params[i])]
-    if(length(temp>100)) 
+    if(length(temp)>25) 
     {
         d <- density(x = temp,na.rm=T)
         plot(d, main=params[i])
@@ -270,42 +301,3 @@ for(i in 1:length(params)) {
 }
 dev.off()
 
-
-
-
-
-
-
-
-
-
-#Link to LAGOSLAKEID sites
-
-lagos_sites_us <- dbGetQuery(con,'select * from limno.lagoslakesites', stringsAsFactors = FALSE) %>% 
-    select(samplesiteid_legacy,lagoslakeid,samplesiteid_lagos)
-
-dat2 <- dat2 %>% left_join(lagos_sites_us)
-# dat3 <- dat2 %>% filter(is.na(lagoslakeid)) %>% 
-#     select(source,OrganizationIdentifier,samplesiteid_legacy) %>% 
-#     distinct()
-# write_csv(dat3,"nolink.csv")
-# dat4 <- dat2 %>% filter(!is.na(lagoslakeid)) %>% 
-#     select(source,OrganizationIdentifier,samplesiteid_legacy) %>% 
-#     distinct()
-# write_csv(dat4,"linked.csv")
-
-
-lagos_variables <- dbGetQuery(con,'select * from limno.lagosvariables_us', stringsAsFactors = FALSE)
-
-write_csv(lagos_variables,"lagos_variables.csv")
-#columns to add
-#variableid_lagos
-#programtableid_lagos
-#comments_lagos
-#censorcode_flag_lagos
-
-#lakeid_legacy
-#methodqualifier_legacy
-#flag_lagos
-#lakeid_nhdid
-#depth_comment_lagos
